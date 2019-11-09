a32fa149de50f8bf165a33502212854cef2e7c20
Adina Pusok <adina.pusok@gmail.com>
2019-11-09 19:04:09 +0000

    Time-stepping: updated the python script for the pure-advection test to plot the integrated quantity vs time steps.

--- FD-PDE/src/tests/python/test_advdiff_advtime.py
+++ FD-PDE/src/tests/python/test_advdiff_advtime.py
@@ -18,10 +18,10 @@ print('# --------------------------------------- #')
 
 n = 100
 tstep = 201
-tout = 50
+tout = 5
 
 # Gaussian shape flags
-gs = ' -L 20.0 -A 10.0 -x0 5.0 -ux 2.0 -dt 1e-2 -adv_scheme 1'
+gs = ' -L 20.0 -A 10.0 -x0 5.0 -ux 2.0 -dt 1e-2 -adv_scheme 0'
 
 # Run test
 # Forward euler
@@ -39,8 +39,16 @@ str3 = '../test_advdiff_advtime.app -pc_type lu -pc_factor_mat_solver_type umfpa
 print(str3)
 os.system(str3)
 
+# Prepare data for time series
+nout = int((tstep-1)/tout+1)
+mass00 = np.zeros(nout)
+mass01 = np.zeros(nout)
+mass02 = np.zeros(nout)
+massan = np.zeros(nout)
+dt = range(0,tstep,tout)
+
 # Plot solution (numerical and analytical)
-fig, axs = plt.subplots(3, 2,figsize=(12,12))
+fig, axs = plt.subplots(2, 2,figsize=(12,9))
 
 for istep in range(0,tstep,tout):
   # Load python module describing data
@@ -91,78 +99,62 @@ for istep in range(0,tstep,tout):
   Tres_an = T_an.reshape(n,m)
   Tx_an = Tres_an[int(n/2),:]
 
-  # subplot(1,1,0)
-  ax0 = axs[0,0]
-  pl0 = ax0.plot(xc,Tx0,label='istep='+str(istep))
-  ax0.set_xlabel('x-dir')
-  ax0.set_ylabel('Q')
-  ax0.set_title('Forward Euler')
-  ax0.legend(title='Qx',loc='upper right')
-  ax0.axis('scaled')
-  ax0.grid(True,color='gray', linestyle='--', linewidth=0.5)
+  # Save mass plot - can do this because it's constant grid spacing
+  ii = int(istep/tout)
+  mass00[ii] = np.sum(Tx0)
+  mass01[ii] = np.sum(Tx1)
+  mass02[ii] = np.sum(Tx2)
+  massan[ii] = np.sum(Tx_an)
 
-  # subplot(1,1,1) - analytical
-  ax1 = axs[0,1]
-  if (istep==0) | (istep==tstep-1):
-    pl1 = ax1.plot(xc,Tx0,label='istep='+str(istep))
-    pl12 = ax1.plot(xc,Tx_an,color='r', linestyle='--',label='istep_an='+str(istep))
-  ax1.set_xlabel('x-dir')
-  ax1.set_ylabel('Q')
-  ax1.legend(title='Qx',loc='upper right')
-  ax1.axis('scaled')
-  ax1.grid(True,color='gray', linestyle='--', linewidth=0.5)
+  if (istep % 50 == 0):
+    # forward euler
+    ax0 = axs[0,0]
+    pl0 = ax0.plot(xc,Tx0,label='istep='+str(istep))
+    if (istep==0) | (istep==tstep-1):
+      pl01 = ax0.plot(xc,Tx_an,color='gray', linestyle='--',label='istep_an='+str(istep))
+    ax0.set_xlabel('x-dir')
+    ax0.set_ylabel('Q')
+    ax0.set_title('Forward Euler')
+    ax0.legend(title='Qx',loc='upper right')
+    ax0.axis('scaled')
+    ax0.grid(True,color='gray', linestyle='--', linewidth=0.5)
 
-  # subplot(2,1,2)
-  ax2 = axs[1,0]
-  pl2 = ax2.plot(xc,Tx1,label='istep='+str(istep))
-  ax2.set_xlabel('x-dir')
-  ax2.set_ylabel('Q')
-  ax2.set_title('Backward Euler')
-  ax2.legend(title='Qx',loc='upper right')
-  ax2.axis('scaled')
-  ax2.grid(True,color='gray', linestyle='--', linewidth=0.5)
+    # backward euler
+    ax1 = axs[0,1]
+    pl1 = ax1.plot(xc,Tx1,label='istep='+str(istep))
+    if (istep==0) | (istep==tstep-1):
+      pl11 = ax1.plot(xc,Tx_an,color='gray', linestyle='--',label='istep_an='+str(istep))
+    ax1.set_xlabel('x-dir')
+    ax1.set_ylabel('Q')
+    ax1.set_title('Backward Euler')
+    ax1.legend(title='Qx',loc='upper right')
+    ax1.axis('scaled')
+    ax1.grid(True,color='gray', linestyle='--', linewidth=0.5)
 
-  # subplot(2,1,3) - analytical
-  ax3 = axs[1,1]
-  if (istep==0) | (istep==tstep-1):
-    pl3 = ax3.plot(xc,Tx1,label='istep='+str(istep))
-    pl13 = ax3.plot(xc,Tx_an,color='r', linestyle='--',label='istep_an='+str(istep))
-  ax3.set_xlabel('x-dir')
-  ax3.set_ylabel('Q')
-  ax3.legend(title='Qx',loc='upper right')
-  ax3.axis('scaled')
-  ax3.grid(True,color='gray', linestyle='--', linewidth=0.5)
+    # crank-nicholson
+    ax2 = axs[1,0]
+    pl2 = ax2.plot(xc,Tx2,label='istep='+str(istep))
+    if (istep==0) | (istep==tstep-1):
+      pl21 = ax2.plot(xc,Tx_an,color='gray', linestyle='--',label='istep_an='+str(istep))
+    ax2.set_xlabel('x-dir')
+    ax2.set_ylabel('Q')
+    ax2.set_title('Crank-Nicholson')
+    ax2.legend(title='Qx',loc='upper right')
+    ax2.axis('scaled')
+    ax2.grid(True,color='gray', linestyle='--', linewidth=0.5)
 
-  # subplot(3,1,4)
-  ax4 = axs[2,0]
-  pl4 = ax4.plot(xc,Tx2,label='istep='+str(istep))
-  ax4.set_xlabel('x-dir')
-  ax4.set_ylabel('Q')
-  ax4.set_title('Crank-Nicholson')
-  ax4.legend(title='Qx',loc='upper right')
-  ax4.axis('scaled')
-  ax4.grid(True,color='gray', linestyle='--', linewidth=0.5)
 
-  # subplot(3,1,5) - analytical
-  ax5 = axs[2,1]
-  if (istep==0) | (istep==tstep-1):
-    pl5 = ax5.plot(xc,Tx2,label='istep='+str(istep))
-    pl15 = ax5.plot(xc,Tx_an,color='r', linestyle='--',label='istep_an='+str(istep))
-  ax5.set_xlabel('x-dir')
-  ax5.set_ylabel('Q')
-  ax5.legend(title='Qx',loc='upper right')
-  ax5.axis('scaled')
-  ax5.grid(True,color='gray', linestyle='--', linewidth=0.5)
-
-  # ax2 = axs[1]
-  # im = ax2.plot(zc,Tz,label='istep='+str(istep))
-  # ax2.set_xlabel('z-dir')
-  # ax2.set_ylabel('Q')
-  # ax2.set_title('Z-section')
-  # ax2.legend(title='Qz',loc='upper left')
-  # ax2.axis('scaled')
-  # # ax2.set(xlim=(xc[0], xc[-1]), ylim=(0, 5))
-  # ax2.grid(True,color='gray', linestyle='--', linewidth=0.5)
-
+# mass plot
+ax3 = axs[1,1]
+pl30 = ax3.plot(dt,massan,color='gray',label='analytical')
+pl31 = ax3.plot(dt,mass00,color='r',label='forward Euler')
+pl32 = ax3.plot(dt,mass01,color='b',label='backward Euler')
+pl33 = ax3.plot(dt,mass02,color='k',label='Crank-Nicholson')
+ax3.set_xlabel('Time steps')
+ax3.set_ylabel('Int(Q)')
+# ax3.set_title('Integrated quantity')
+ax3.legend(loc='upper right')
+# ax3.axis('scaled')
+ax3.grid(True,color='gray', linestyle='--', linewidth=0.5)
 
 plt.savefig(fname+'.pdf')
