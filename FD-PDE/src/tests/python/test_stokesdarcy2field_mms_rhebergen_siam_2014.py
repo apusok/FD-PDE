c25d61f62cc09b3c9e5379a577f1741d7d656e89
Yuan <yuanli.ac@gmail.com>
2020-08-27 12:29:55 +0100

    Stokes and DarcyStokes: create a new BCtype, BC_DIRICHLET_TRUE, for the new implementation of Dirichlet conditions. * Notes and benchmark results for comparison will be shared in slack; * We may discuss later if we need the same implementation for the adv-diff model; * We may discuss later if we want to only keep one implementation; * We may discuss later if Dave is not satisfied with my explanation in the comments regarding to the need of adjusting x/z coord mannually for each single test.

--- FD-PDE/src/tests/python/test_stokesdarcy2field_mms_rhebergen_siam_2014.py
+++ FD-PDE/src/tests/python/test_stokesdarcy2field_mms_rhebergen_siam_2014.py
@@ -15,110 +15,117 @@ f1 = 'out_mms_rhebergen_2014_solution'
 
 # Parameters
 n = [25, 40, 50, 80, 100, 125, 150, 200, 300, 400]
-alpha = 1.0 # [-1.0/3.0, 0, 1, 1e1, 1e3, 1e6]
+alpha = [-1.0/3.0, 0, 1, 1e1, 1e3, 1e6]
+lc = ['y','m','c','r','g','b',]
 
 # Run simulations
-for nx in n:
-  # Create output filename
-  fout1 = f1+'_'+str(nx)+'.out'
+for al in alpha:
+    for nx in n:
+      # Create output filename
+      i = alpha.index(al)
+      fout1 = f1+'_'+str(nx)+'_'+str(i)+'.out'
+    
+      # Run with different resolutions
+      str1 = '../test_stokesdarcy2field_rhebergen-siam-2014.app -pc_type lu -pc_factor_mat_solver_type umfpack -alpha '+str(al)+' -nx '+str(nx)+' -nz '+str(nx)+' > '+fout1
+      print(str1)
+      os.system(str1)
+    
+      # Get mms solution data
+      f1out = 'out_mms_solution'
+      imod = importlib.import_module(f1out)
+      data = imod._PETScBinaryLoad()
+      imod._PETScBinaryLoadReportNames(data)
+    
+      mx = data['Nx'][0]
+      mz = data['Ny'][0]
+      uxmms = data['X_face_x']
+      uzmms= data['X_face_y']
+      pmms = data['X_cell']
+    
+      # Get solution data
+      f1out = 'out_solution'
+      imod = importlib.import_module(f1out)
+      data = imod._PETScBinaryLoad()
+      imod._PETScBinaryLoadReportNames(data)
+    
+      x = data['x1d_vertex']
+      z = data['y1d_vertex']
+      xc = data['x1d_cell']
+      zc = data['y1d_cell']
+      ux = data['X_face_x']
+      uz= data['X_face_y']
+      p = data['X_cell']
+    
+      # Plot data
+      fig = plt.figure(1,figsize=(12,12))
+    
+      ax = plt.subplot(331)
+      im = ax.imshow(pmms.reshape(mz,mx),extent=[min(xc), max(xc), min(zc), max(zc)])
+      ax.set_title('MMS P')
+      cbar = fig.colorbar(im,ax=ax, shrink=0.75)
+    
+      ax = plt.subplot(332)
+      im = ax.imshow(p.reshape(mz,mx),extent=[min(xc), max(xc), min(zc), max(zc)])
+      ax.set_title('Numerical P')
+      cbar = fig.colorbar(im,ax=ax, shrink=0.75)
+    
+      ax = plt.subplot(333)
+      im = ax.imshow(pmms.reshape(mz,mx)-p.reshape(mz,mx),extent=[min(xc), max(xc), min(zc), max(zc)])
+      ax.set_title('Error P')
+      cbar = fig.colorbar(im,ax=ax, shrink=0.75)
+    
+      ax = plt.subplot(334)
+      im = ax.imshow(uxmms.reshape(mz,mx+1),extent=[min(x), max(x), min(zc), max(zc)])
+      ax.set_title('MMS ux')
+      cbar = fig.colorbar(im,ax=ax, shrink=0.75)
+    
+      ax = plt.subplot(335)
+      im = ax.imshow(ux.reshape(mz,mx+1),extent=[min(x), max(x), min(zc), max(zc)])
+      ax.set_title('Numerical ux')
+      cbar = fig.colorbar(im,ax=ax, shrink=0.75)
+    
+      ax = plt.subplot(336)
+      im = ax.imshow(uxmms.reshape(mz,mx+1)-ux.reshape(mz,mx+1),extent=[min(x), max(x), min(zc), max(zc)])
+      ax.set_title('Error ux')
+      cbar = fig.colorbar(im,ax=ax, shrink=0.75)
+    
+      ax = plt.subplot(337)
+      im = ax.imshow(uzmms.reshape(mz+1,mx),extent=[min(xc), max(xc), min(z), max(z)])
+      ax.set_title('MMS uz')
+      cbar = fig.colorbar(im,ax=ax, shrink=0.75)
+    
+      ax = plt.subplot(338)
+      im = ax.imshow(uz.reshape(mz+1,mx),extent=[min(xc), max(xc), min(z), max(z)])
+      ax.set_title('Numerical uz')
+      cbar = fig.colorbar(im,ax=ax, shrink=0.75)
+    
+      ax = plt.subplot(339)
+      im = ax.imshow(uzmms.reshape(mz+1,mx)-uz.reshape(mz+1,mx),extent=[min(xc), max(xc), min(z), max(z)])
+      ax.set_title('Error uz')
+      cbar = fig.colorbar(im,ax=ax, shrink=0.75)
+      plt.savefig(f1+'_nx_'+str(nx)+'_alpha_'+str(al)+'.pdf')
+      plt.close()
 
-  # Run with different resolutions
-  str1 = '../test_stokesdarcy2field_rhebergen-siam-2014.app -pc_type lu -pc_factor_mat_solver_type umfpack -alpha '+str(alpha)+' -nx '+str(nx)+' -nz '+str(nx)+' > '+fout1
-  print(str1)
-  os.system(str1)
-
-  # Get mms solution data
-  f1out = 'out_mms_solution'
-  imod = importlib.import_module(f1out)
-  data = imod._PETScBinaryLoad()
-  imod._PETScBinaryLoadReportNames(data)
-
-  mx = data['Nx'][0]
-  mz = data['Ny'][0]
-  uxmms = data['X_face_x']
-  uzmms= data['X_face_y']
-  pmms = data['X_cell']
-
-  # Get solution data
-  f1out = 'out_solution'
-  imod = importlib.import_module(f1out)
-  data = imod._PETScBinaryLoad()
-  imod._PETScBinaryLoadReportNames(data)
-
-  x = data['x1d_vertex']
-  z = data['y1d_vertex']
-  xc = data['x1d_cell']
-  zc = data['y1d_cell']
-  ux = data['X_face_x']
-  uz= data['X_face_y']
-  p = data['X_cell']
-
-  # Plot data
-  fig = plt.figure(1,figsize=(12,12))
-
-  ax = plt.subplot(331)
-  im = ax.imshow(pmms.reshape(mz,mx),extent=[min(xc), max(xc), min(zc), max(zc)])
-  ax.set_title('MMS P')
-  cbar = fig.colorbar(im,ax=ax, shrink=0.75)
-
-  ax = plt.subplot(332)
-  im = ax.imshow(p.reshape(mz,mx),extent=[min(xc), max(xc), min(zc), max(zc)])
-  ax.set_title('Numerical P')
-  cbar = fig.colorbar(im,ax=ax, shrink=0.75)
-
-  ax = plt.subplot(333)
-  im = ax.imshow(pmms.reshape(mz,mx)-p.reshape(mz,mx),extent=[min(xc), max(xc), min(zc), max(zc)])
-  ax.set_title('Error P')
-  cbar = fig.colorbar(im,ax=ax, shrink=0.75)
-
-  ax = plt.subplot(334)
-  im = ax.imshow(uxmms.reshape(mz,mx+1),extent=[min(x), max(x), min(zc), max(zc)])
-  ax.set_title('MMS ux')
-  cbar = fig.colorbar(im,ax=ax, shrink=0.75)
-
-  ax = plt.subplot(335)
-  im = ax.imshow(ux.reshape(mz,mx+1),extent=[min(x), max(x), min(zc), max(zc)])
-  ax.set_title('Numerical ux')
-  cbar = fig.colorbar(im,ax=ax, shrink=0.75)
-
-  ax = plt.subplot(336)
-  im = ax.imshow(uxmms.reshape(mz,mx+1)-ux.reshape(mz,mx+1),extent=[min(x), max(x), min(zc), max(zc)])
-  ax.set_title('Error ux')
-  cbar = fig.colorbar(im,ax=ax, shrink=0.75)
-
-  ax = plt.subplot(337)
-  im = ax.imshow(uzmms.reshape(mz+1,mx),extent=[min(xc), max(xc), min(z), max(z)])
-  ax.set_title('MMS uz')
-  cbar = fig.colorbar(im,ax=ax, shrink=0.75)
-
-  ax = plt.subplot(338)
-  im = ax.imshow(uz.reshape(mz+1,mx),extent=[min(xc), max(xc), min(z), max(z)])
-  ax.set_title('Numerical uz')
-  cbar = fig.colorbar(im,ax=ax, shrink=0.75)
-
-  ax = plt.subplot(339)
-  im = ax.imshow(uzmms.reshape(mz+1,mx)-uz.reshape(mz+1,mx),extent=[min(xc), max(xc), min(z), max(z)])
-  ax.set_title('Error uz')
-  cbar = fig.colorbar(im,ax=ax, shrink=0.75)
-  plt.savefig(f1+'_nx_'+str(nx)+'.pdf')
-  plt.close()
-
-  # output solution - general 
-#   dmout.general_output_imshow('out_mms_solution',None,None)
-#   dmout.general_output_imshow('out_solution',None,None)
-#   f1out = 'out_mms_solution_cell_dof0'
-#   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
-#   f1out = 'out_mms_solution_face0_dof0'
-#   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
-#   f1out = 'out_mms_solution_face1_dof0'
-#   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
-#   f1out = 'out_solution_cell_dof0'
-#   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
-#   f1out = 'out_solution_face0_dof0'
-#   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
-#   f1out = 'out_solution_face1_dof0'
-#   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
-
+    # output solution - general 
+    #   dmout.general_output_imshow('out_mms_solution',None,None)
+    #   dmout.general_output_imshow('out_solution',None,None)
+    #   f1out = 'out_mms_solution_cell_dof0'
+    #   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
+    #   f1out = 'out_mms_solution_face0_dof0'
+    #   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
+    #   f1out = 'out_mms_solution_face1_dof0'
+    #   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
+    #   f1out = 'out_solution_cell_dof0'
+    #   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
+    #   f1out = 'out_solution_face0_dof0'
+    #   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
+    #   f1out = 'out_solution_face1_dof0'
+    #   os.system('mv '+f1out+'.pdf '+f1out+'_'+str(nx)+'.pdf')
+    
+# Plot convergence data
+plt.figure(1,figsize=(6,6))
+plt.grid(color='lightgray', linestyle=':')
+    
 # Norm variables
 nrm1v = np.zeros(len(n))
 nrm1vx = np.zeros(len(n))
@@ -127,37 +134,47 @@ nrm1p = np.zeros(len(n))
 hx = np.zeros(len(n))
 
 # Parse output and save norm info
-for i in range(0,len(n)):
-    nx = n[i]
-
-    fout1 = f1+'_'+str(nx)+'.out'
-
-    # Open file 1 and read
-    f = open(fout1, 'r')
-    for line in f:
-      if 'Velocity:' in line:
-          nrm1v[i] = float(line[20:38])
-          nrm1vx[i] = float(line[48:66])
-          nrm1vz[i] = float(line[76:94])
-      if 'Pressure:' in line:
-          nrm1p[i] = float(line[20:38])
-      if 'Grid info:' in line:
-          hx[i] = float(line[18:36])
-    f.close()
-
+for j in range(0, len(alpha)):
+    for i in range(0,len(n)):
+        nx = n[i]
+    
+        fout1 = f1+'_'+str(nx)+'_'+str(j)+'.out'
+    
+        # Open file 1 and read
+        f = open(fout1, 'r')
+        for line in f:
+          if 'Velocity:' in line:
+              nrm1v[i] = float(line[20:38])
+              nrm1vx[i] = float(line[48:66])
+              nrm1vz[i] = float(line[76:94])
+          if 'Pressure:' in line:
+              nrm1p[i] = float(line[20:38])
+          if 'Grid info:' in line:
+              hx[i] = float(line[18:36])
+        f.close()
+    
+    plt.plot(np.log10(hx),np.log10(nrm1v),lc[j]+'+--',label='v:'+str(alpha[j]))
+    plt.plot(np.log10(hx),np.log10(nrm1p),lc[j]+'o--')
+    
+    # Print convergence orders:
+    hx_log    = np.log10(hx)
+    nrm1v_log = np.log10(nrm1v)
+    nrm1p_log = np.log10(nrm1p)
+    
+    # Perform linear regression
+    sl1v, intercept, r_value, p_value, std_err = linregress(hx_log, nrm1v_log)
+    sl1p, intercept, r_value, p_value, std_err = linregress(hx_log, nrm1p_log)
+    
+    print('# --------------------------------------- #')
+    print('# MMS StokesDarcy2Field (Rhebergen et al. SIAM 2014) convergence order:')
+    print('     v_slope = '+str(sl1v)+' p_slope = '+str(sl1p))
+    
 x1 = [-2.6, -1.6]
 y1 = [-6, -5]
-y2 = [-6, -4]
+y2 = [-6, -4]    
 
-# Plot convergence data
-plt.figure(1,figsize=(6,6))
-
-plt.grid(color='lightgray', linestyle=':')
-plt.plot(np.log10(hx),np.log10(nrm1v),'k+--',label='v')
-plt.plot(np.log10(hx),np.log10(nrm1p),'ko--',label='P')
-
-plt.plot(x1,y1,'r-',label='slope=1')
-plt.plot(x1,y2,'b-',label='slope=2')
+plt.plot(x1,y1,'k--',label='slope=1')
+plt.plot(x1,y2,'k-',label='slope=2')
 
 plt.xlabel('log10(h)',fontweight='bold',fontsize=12)
 plt.ylabel('log10||e||',fontweight='bold',fontsize=12)
@@ -165,19 +182,7 @@ plt.legend()
 
 plt.savefig(f1+'.pdf')
 plt.close()
-
-# Print convergence orders:
-hx_log    = np.log10(hx)
-nrm1v_log = np.log10(nrm1v)
-nrm1p_log = np.log10(nrm1p)
-
-# Perform linear regression
-sl1v, intercept, r_value, p_value, std_err = linregress(hx_log, nrm1v_log)
-sl1p, intercept, r_value, p_value, std_err = linregress(hx_log, nrm1p_log)
-
-print('# --------------------------------------- #')
-print('# MMS StokesDarcy2Field (Rhebergen et al. SIAM 2014) convergence order:')
-print('     v_slope = '+str(sl1v)+' p_slope = '+str(sl1p))
+    
 
 os.system('rm -r __pycache__')
 
