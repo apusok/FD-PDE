eb13c7a97e32ef694ce74a0c657e6042d60326ce
Adina Pusok <adina.pusok@gmail.com>
2020-10-23 15:34:19 +0100

    [BC Dirichlet] Updated the error message for BC_DIRICHLET/BC_DIRICHLET_STAG to be more explicit

--- FD-PDE/src/fdpde_advdiff_residual.c
+++ FD-PDE/src/fdpde_advdiff_residual.c
@@ -288,7 +288,7 @@ PetscErrorCode DMStagBCListApply_AdvDiff(DM dm, Vec xlocal,DM dmcoeff, Vec coeff
   for (ibc = 0; ibc<nbc; ibc++) {
 
     if (bclist[ibc].type == BC_DIRICHLET) {
-      SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"BC type DIRICHLET on the true boundary for FDPDE_ADVDIFF [ELEMENT] is not yet implemented.");
+      SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"BC_DIRICHLET type on the true boundary for FDPDE_ADVDIFF [ELEMENT] is not yet implemented. Use BC_DIRICHLET_STAG type instead!");
     }
     
     if (bclist[ibc].type == BC_DIRICHLET_STAG) {
